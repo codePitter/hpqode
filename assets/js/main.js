6bb719a727d0f4e07013bbd7255917df79dcdf9b
Pitter <pitterbck@gmail.com>
2021-10-10 00:21:51 -0300

    Finalizada card Courses (falta logros)

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,7 @@ function videoStatus(videoId, btnId){
         btn.innerHTML = "Play <i class='fa fa-play'></i>";
     }
 }
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -37,15 +38,34 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  slides[slideIndex-1].style.display = "block";
+}
+
+var slideIndex = 1;
+showSlides2(slideIndex);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
